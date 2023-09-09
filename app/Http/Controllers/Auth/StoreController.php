f8f235085a38a33c736c659e9cfa0c23a152708f
Khurshiddd <marifovxurshid80@gmail.com>
2023-09-09 15:52:54 +0500

    add videoresource

--- app/Http/Controllers/Auth/StoreController.php
+++ app/Http/Controllers/Auth/StoreController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreRequest;
 use App\Models\User;
+use Illuminate\Support\Str;
 
 class StoreController extends Controller
 {
-    public function __invoke(StoreRequest $request)
-    {   
+    public function __invoke(StoreRequest $request): \Illuminate\Http\JsonResponse
+    {
         $user = $request->validated();
         $user['email_verified_at'] = now();
         $user['remember_token'] = Str::random(10);
--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\VideoResource;
 use App\Models\Video;
 use App\Http\Requests\StoreVideoRequest;
 use App\Http\Requests\UpdateVideoRequest;
@@ -10,10 +11,10 @@ use Illuminate\Support\Facades\Storage;
 
 class VideoController extends Controller
 {
-    public function index(): \Illuminate\Foundation\Application|\Illuminate\Http\Response|\Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory
+    public function index(): \Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
         $videos = Video::all();
-        return response($videos);
+        return VideoResource::collection($videos);
     }
 
     public function store(StoreVideoRequest $request): \Illuminate\Http\JsonResponse
